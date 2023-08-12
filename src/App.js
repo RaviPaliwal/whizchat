22c4d35373b80072c2c2d1a35bb76b8940cf79a7
Ravi Paliwal <ravipaliwal885078@gmail.com>
2023-08-12 12:16:43 +0530

    Routing Applied

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import LoginPage from './Components/LoginPage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" index element={<LoginPage />} />
+        <Route path="/" index element={< LoginPage />}/>
         {/* <Route path="/"  element={<HomePage />} /> */}
         {/* Add more routes here if needed */}
+
       </Routes>
     </BrowserRouter>
   );
