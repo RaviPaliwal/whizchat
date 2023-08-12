47057ea8f1ee177af11a124c484c83ca6686709a
Ravi Paliwal <ravipaliwal885078@gmail.com>
2023-08-12 15:49:16 +0530

    Updates

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,79 +1,69 @@
-import React from "react";
-import {
-  CssBaseline,
-  createTheme,
-  ThemeProvider,
-  Container,
-  Paper,
-  Typography,
-  Tabs,
-  Tab,
-  List,
-  ListItem,
-  ListItemAvatar,
-  Avatar,
-  ListItemText,
-  TextField,
-} from "@mui/material";
+import React from 'react';
+import { CssBaseline, createTheme, ThemeProvider, Container, Paper, Typography, Tabs, Tab, List, ListItem, ListItemAvatar, Avatar, ListItemText, TextField } from '@mui/material';
 import "../Assets/Styles/HomePage.css";
-import { AccountCircle, FiberManualRecord, Chat } from "@mui/icons-material";
+import { AccountCircle, FiberManualRecord, Chat } from '@mui/icons-material';
 import SendIcon from "@mui/icons-material/Send";
 import InputAdornment from "@mui/material/InputAdornment";
 
+// Create a custom theme
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#683EF7",
+      main: '#683EF7',
     },
     secondary: {
       main: "#683EF7",
     },
     background: {
-      default: "#090912",
+      default: '#090912'
     },
     text: {
-      primary: "#683EF7",
+      primary: '#683EF7',
       secondary: "white",
     },
   },
 });
 
+// Define the styles for the app container, sidebar, chat section, and chat area
 const appContainerStyle = {
-  display: "flex",
-  height: "100vh", // 100% of the viewport height
-  maxWidth: "100vw", // 100% of the viewport width
-  overflow: "hidden", // Hide overflow from overflowing content
+  display: 'flex',
+  height: '100vh',
+  maxWidth: '100vw',
+  overflow: 'hidden',
 };
 
 const sidebarStyle = {
-  width: "6rem", // Fixed sidebar width
-  backgroundColor: theme.palette.background.default,
-  padding: ".08rem",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  color: "white",
+  width: '6rem',
+  backgroundColor:  theme.palette.background.default,
+  padding: '.08rem',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+  color: 'white',
 };
 
 const chatSectionStyle = {
-  flexGrow: 1, // Fill the remaining space
-  display: "flex",
-  flexDirection: "column",
-  padding: ".8rem",
+  flexGrow: 1,
+  display: 'flex',
+  flexDirection: 'column',
+  padding: '.8rem',
+  borderRadius:"0px"
 };
 
 const chatAreaStyle = {
   ...chatSectionStyle,
-  backgroundColor: "", // Change the background color
+  backgroundColor: "",
   color: "primary",
 };
 
 const listItemStyle = {
   border: "none",
   borderRadius: "15px",
+  backgroundImage:"linear-gradient(135deg, rgba(157, 65, 225, 0.7), rgba(104, 62, 247, 0.6))",
   marginBottom: "5px",
   height: "5rem",
+  backgroundColor:"#683EF7",
   width: "100%",
   "@media (max-width: 600px)": {
     height: "auto",
@@ -93,22 +83,16 @@ function HomePage() {
             value={0}
             textColor="secondary"
             indicatorColor="secondary"
-            style={{ marginBottom: "16px" }}
+            style={{ marginBottom: '16px' }}
           >
-            <Tab
-              label="Home"
-              icon={<AccountCircle className="customNavicon " />}
-            />
-            <Tab
-              label="Status"
-              icon={<FiberManualRecord className="customNavicon" />}
-            />
-            <Tab label="New Chat" icon={<Chat className="customNavicon" />} />
+            <Tab label="Home" icon={<AccountCircle className='customNavicon ' />} />
+            <Tab label="Status" icon={<FiberManualRecord className='customNavicon' />} />
+            <Tab label="New Chat" icon={<Chat className='customNavicon' />} />
           </Tabs>
         </Paper>
 
         {/* Chat profiles */}
-        <Paper style={{ ...chatSectionStyle }}>
+        <Paper style={{ ...chatSectionStyle, maxWidth:"28rem", }}>
           {/* Display selected user's profile */}
           <Typography variant="h6" gutterBottom>
             Chats
@@ -117,10 +101,7 @@ function HomePage() {
           <List>
             <ListItem sx={{ ...listItemStyle }} component="button">
               <ListItemAvatar>
-                <Avatar
-                  alt="userimg1"
-                  src="url(https://source.unsplash.com/random?wallpapers)"
-                />
+                <Avatar alt="userimg1" src="https://source.unsplash.com/random?wallpapers" />
               </ListItemAvatar>
               <ListItemText
                 primary="Ravi Paliwal"
@@ -129,10 +110,7 @@ function HomePage() {
             </ListItem>
             <ListItem sx={{ ...listItemStyle }} component="button">
               <ListItemAvatar>
-                <Avatar
-                  alt="User 2"
-                  src="url(https://source.unsplash.com/random?wallpapers)"
-                />
+                <Avatar alt="User 2" src="https://source.unsplash.com/random?wallpapers" />
               </ListItemAvatar>
               <ListItemText
                 sx={{ overflowX: "clip" }}
@@ -161,7 +139,6 @@ function HomePage() {
               ),
             }}
           />
-          {/* <TextField  label="Type a message" variant="outlined" /> */}
         </Paper>
       </Container>
     </ThemeProvider>
--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,148 +1,268 @@
-import React from 'react';
-import { CssBaseline, createTheme, ThemeProvider, Container, Paper, Typography, Tabs, Tab, List, ListItem, ListItemAvatar, Avatar, ListItemText, TextField } from '@mui/material';
-import "../Assets/Styles/HomePage.css";
-import { AccountCircle, FiberManualRecord, Chat } from '@mui/icons-material';
-import SendIcon from "@mui/icons-material/Send";
-import InputAdornment from "@mui/material/InputAdornment";
+import React from "react";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Box from "@mui/material/Box";
+import logo from "../Assets/Images/logo.png";
+import "../Assets/Styles/Login.css";
+import { useNavigate } from "react-router-dom";
 
-// Create a custom theme
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#683EF7',
+export default function LoginPage() {
+  const [showSignup, setShowSignup] = React.useState(false);
+  const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+  };
+
+  const toggleSignup = (e) => {
+    e.preventDefault();
+    setShowSignup(!showSignup);
+  };
+  const mytheme = createTheme({
+    palette: {
+      primary: {
+        main: "#683EF7", // Primary color
+      },
     },
-    secondary: {
-      main: "#683EF7",
-    },
-    background: {
-      default: '#090912'
-    },
-    text: {
-      primary: '#683EF7',
-      secondary: "white",
-    },
-  },
-});
+  });
 
-// Define the styles for the app container, sidebar, chat section, and chat area
-const appContainerStyle = {
-  display: 'flex',
-  height: '100vh',
-  maxWidth: '100vw',
-  overflow: 'hidden',
-};
-
-const sidebarStyle = {
-  width: '6rem',
-  backgroundColor:  theme.palette.background.default,
-  padding: '.08rem',
-  display: 'flex',
-  flexDirection: 'column',
-  alignItems: 'center',
-  justifyContent: 'center',
-  color: 'white',
-};
-
-const chatSectionStyle = {
-  flexGrow: 1,
-  display: 'flex',
-  flexDirection: 'column',
-  padding: '.8rem',
-  borderRadius:"0px"
-};
-
-const chatAreaStyle = {
-  ...chatSectionStyle,
-  backgroundColor: "",
-  color: "primary",
-};
-
-const listItemStyle = {
-  border: "none",
-  borderRadius: "15px",
-  backgroundImage:"linear-gradient(135deg, rgba(157, 65, 225, 0.7), rgba(104, 62, 247, 0.6))",
-  marginBottom: "5px",
-  height: "5rem",
-  backgroundColor:"#683EF7",
-  width: "100%",
-  "@media (max-width: 600px)": {
-    height: "auto",
-  },
-};
-
-function HomePage() {
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider theme={mytheme}>
       <CssBaseline />
-      <Container maxWidth={false} disableGutters style={appContainerStyle}>
-        {/* Sidebar */}
-        <Paper style={sidebarStyle}>
-          <Tabs
-            orientation="vertical"
-            variant="scrollable"
-            value={0}
-            textColor="secondary"
-            indicatorColor="secondary"
-            style={{ marginBottom: '16px' }}
-          >
-            <Tab label="Home" icon={<AccountCircle className='customNavicon ' />} />
-            <Tab label="Status" icon={<FiberManualRecord className='customNavicon' />} />
-            <Tab label="New Chat" icon={<Chat className='customNavicon' />} />
-          </Tabs>
-        </Paper>
-
-        {/* Chat profiles */}
-        <Paper style={{ ...chatSectionStyle, maxWidth:"28rem", }}>
-          {/* Display selected user's profile */}
-          <Typography variant="h6" gutterBottom>
-            Chats
-          </Typography>
-          {/* Profile details */}
-          <List>
-            <ListItem sx={{ ...listItemStyle }} component="button">
-              <ListItemAvatar>
-                <Avatar alt="userimg1" src="https://source.unsplash.com/random?wallpapers" />
-              </ListItemAvatar>
-              <ListItemText
-                primary="Ravi Paliwal"
-                secondary="Welcome to WhizChat!"
-              />
-            </ListItem>
-            <ListItem sx={{ ...listItemStyle }} component="button">
-              <ListItemAvatar>
-                <Avatar alt="User 2" src="https://source.unsplash.com/random?wallpapers" />
-              </ListItemAvatar>
-              <ListItemText
-                sx={{ overflowX: "clip" }}
-                primary="Shashwat Tiwari"
-                secondary="Hey How are you?🙋‍♂️"
-              />
-            </ListItem>
-            {/* Add more dummy user profiles */}
-          </List>
-        </Paper>
-        {/* Chat area */}
-        <Paper style={chatAreaStyle}>
-          {/* Display chat messages */}
-          <Typography variant="h6" gutterBottom>
-            Chat Area
-          </Typography>
-          {/* Chat messages */}
-          <TextField
-            label="Type a message"
-            variant="outlined"
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <SendIcon />
-                </InputAdornment>
-              ),
+      <Grid container sx={{ height: "100vh" }}>
+        <Grid
+          item
+          xs={12}
+          md={6}
+          sx={{
+            position: "relative",
+            backgroundImage:
+              "url(https://source.unsplash.com/random?wallpapers)",
+            backgroundRepeat: "no-repeat",
+            backgroundColor: (t) =>
+              t.palette.mode === "light"
+                ? t.palette.grey[50]
+                : t.palette.grey[900],
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            overflow: "hidden", // Hide overflowing text
+          }}
+        >
+          <div
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              textAlign: "center",
+              padding: "16px",
+              background: "rgba(255, 255, 255, 0.7)",
+              borderRadius: "8px",
             }}
-          />
-        </Paper>
-      </Container>
+          >
+            <Typography variant="h4" gutterBottom>
+              Welcome to The Ai Powered Chat App!
+            </Typography>
+            <Typography variant="body1">
+              😍😍 Join the conversation and connect with others smartly !
+            </Typography>
+          </div>
+        </Grid>
+
+        {/* Signup Grid */}
+        {showSignup ? (
+          <Grid
+            px={6}
+            item
+            xs={12}
+            md={6}
+            component={Paper}
+            elevation={6}
+            square
+          >
+            {/* Signup Form */}
+            {/* ... */}
+            <Box
+              sx={{
+                my: 8,
+                mx: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <img
+                className="logoAnimation"
+                style={{ width: "60%" }}
+                src={logo}
+                alt="logo"
+              />
+              <Typography component="h1" variant="h5">
+                Signup
+              </Typography>
+              <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 1 }}
+              >
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="mobile"
+                  label="Mobile"
+                  name="mobile"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  onClick={(e) => {
+                    e.preventDefault();
+                    navigate("/chats");
+                  }}
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign Up
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="#" variant="body2">
+                      Privacy policy
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="#" onClick={toggleSignup} variant="body2">
+                      Already have an account? Log in
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+          </Grid>
+        ) : (
+          <Grid
+            item
+            px={6}
+            xs={12}
+            md={6}
+            component={Paper}
+            elevation={6}
+            square
+          >
+            {/* Login Form */}
+            <Box
+              sx={{
+                my: 8,
+                mx: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <img
+                className="logoAnimation"
+                style={{ width: "60%" }}
+                src={logo}
+                alt="logo"
+              />
+              <Typography component="h1" variant="h5">
+                Login
+              </Typography>
+              <Box
+                component="form"
+                noValidate
+                onSubmit={handleSubmit}
+                sx={{ mt: 1 }}
+              >
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Sign In
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="#" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="#" onClick={toggleSignup} variant="body2">
+                      Don't have an account? Sign Up
+                    </Link>
+                  </Grid>
+                </Grid>
+              </Box>
+            </Box>
+            {/* ... */}
+          </Grid>
+        )}
+      </Grid>
     </ThemeProvider>
   );
 }
-
-export default HomePage;
