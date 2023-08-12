26286018b4afc8ea763cf62b343215de1bb9a369
Ravi Paliwal <ravipaliwal885078@gmail.com>
2023-08-12 15:42:26 +0530

    Chat Page Created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import LoginPage from './Components/LoginPage';
+import HomePage from './Components/HomePage';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" index element={< LoginPage />}/>
-        {/* <Route path="/"  element={<HomePage />} /> */}
+        <Route path="/chats" element={<HomePage/>}/>
         {/* Add more routes here if needed */}
 
       </Routes>
